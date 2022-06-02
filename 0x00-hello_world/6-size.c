c1f4f261dac514c1dbabf6603a0e965eb2e49fdc
OttoNyongole <nyongole.otto@gmail.com>
2022-06-02 14:31:19 -0700

    printing size of variables using sizeof function

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+int a;
+float b;
+double d;
+char c;
+/*sizeof evaluates the size of a variable*/
+printf("Size of int: %zu bytes\n", sizeof(a));
+printf("Size of float: %zu bytes\n", sizeof(b));
+printf("Size of double: %zu bytes\n", sizeof(d);
+printf("Size of char: %zu byte\n", sizeof(c));
+return 0
+}
