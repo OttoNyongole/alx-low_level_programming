bd9de4f48a345898765bc7aebe8bdcab88ac86ac
OttoNyongole <nyongole.otto@gmail.com>
2022-06-02 20:58:30 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,2 @@
-#include<stdio.h>
-int main() {
-int a;
-float b;
-double d;
-char c;
-/*sizeof evaluates the size of a variable*/
-printf("Size of int: %zu bytes\n", sizeof(a));
-printf("Size of float: %zu bytes\n", sizeof(b));
-printf("Size of double: %zu bytes\n", sizeof(d);
-printf("Size of char: %zu byte\n", sizeof(c));
-return 0
-}
+#!/bin/bash
+gcc -S -m32 $CFILE
