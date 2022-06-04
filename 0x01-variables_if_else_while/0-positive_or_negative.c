fde66f711f66cf53888daf7e3893d6282f2c76a8
OttoNyongole <nyongole.otto@gmail.com>
2022-06-03 19:58:38 -0700

    Printing rondom neg, pos and zero numbers

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* program to print positive and negative number rondomly*/
+
+/*Initialization of the function main*/
+int main (void)
+{
+		int n;
+
+			srand(time(0));
+			n = rand() - RAND_MAX / 2;
+			if (n=0; n<0; n++)
+			{
+			printf("The value of n is negative: %d\n", n);
+			else if (n>0)
+			{
+			printf("The value of n is positive: %d\n",n);
+			}
+			else {
+			printf("The value of n is zero %d\n",n);
+			}
+			}
+		return (0);
+}
