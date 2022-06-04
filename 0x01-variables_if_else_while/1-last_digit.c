8ea9693bb7b1057c968468a11d430fb70a5627ce
OttoNyongole <nyongole.otto@gmail.com>
2022-06-04 13:13:29 -0700

    rondom numbers

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include <stdio.h>
+#include<conio.h>
+#include <stdlib.h>
+/**Declaration of function main
+ *main function is the first function to be excuted by the program
+ */
+int main ()
+{
+	printf("The rondom number is: %d",rand());
+	printf("The rondom number is: %d",rand());
+	printf("The rondom number is: %d",rand());
+	printf("The rondom number is: %d",rand());
+
+return 0;
+}
+
