33dd3af9c1345f8fe021a60ef653de48208ad09c
OttoNyongole <nyongole.otto@gmail.com>
2022-06-05 14:02:11 -0700

    possible combination of double digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+/**
+ * c file printing combination of dought digit
+ *
+ */
+
+#include <stdio.h>
+
+/**
+ * main - a program that prints all possible 
+ * different combinations of two digits
+ *
+ * Return:Always zero
+ */
+
+int main(void)
+{
+	int i, j;
+for (i = 0; i <=9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+		putchar((i % 10) + '0');
+		putchar((j % 10) + '0)';
+		if (i == 9 && j == 9)
+			continue;
+		putchar(',');
+		putcha(' ');
+		}
+	}
+	
+	putchar('\n');
+return (0);
+}
