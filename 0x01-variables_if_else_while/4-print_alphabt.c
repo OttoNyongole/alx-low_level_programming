7225769c309ad281484acc1ee89a15c37bb5b7f2
OttoNyongole <nyongole.otto@gmail.com>
2022-06-05 11:18:16 -0700

    printing alphabet excluding latters e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,24 @@
 /**
- * To print alphabe in lowercase excluding e and q
+ * To print alphabe in lowercase 
+ * excluding e and q
  * using a putchar statement only
  */
 #include <stdio.h>
 
 /**
- * Mian function to initialize the excution of the program
- *Using putchar statement we are going to print lowercase alphabet excluding eand q
+ *Mian function to initialize the 
+ *excution of the program
+ *Using putchar statement we are going to print 
+ lowercase alphabet excluding eand q
  *Return is zero
  */
-int main (void)
+int main(void)
 {
 	char letters;
 /*for loop with if statement*/
 	for (letters = 'a'; letters <= 'z'; letters++)
 	{
-		if (letters !='e' && letters !='q')
+		if (letters != 'e' && letters != 'q')
 			putchar (letters);
 	}
 	putchar ('\n');
