1e5e3641d1f5666b6754fd2dc1fc911ae4deb640
OttoNyongole <nyongole.otto@gmail.com>
2022-06-05 02:59:13 -0700

    printing alphabet lowcase using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+/**C program to print all upper case and lower case alphabets.
+ *  * using the putchar function
+ *  */
+ 
+#include <stdio.h>
+#include <stdio.h>
+ /* The main function*/
+int main()
+{
+	char i;
+	for(i='a'; i<='z'; i++)
+	putchar("\nLower case characters:\n");
+	return 0;
+}
+
