8722f8b39112d5de76ef30724d4024c135106b77
OttoNyongole <nyongole.otto@gmail.com>
2022-06-05 13:21:29 -0700

    printing hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -16,7 +16,7 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 		putchar((i % 10) + '0');
-	for (letters = 'a' letters <= 'f'; letters++)
+	for (letters = 'a'; letters <= 'f'; letters++)
 		putchar(letters);
 	putchar('\n');
 	return (0);
