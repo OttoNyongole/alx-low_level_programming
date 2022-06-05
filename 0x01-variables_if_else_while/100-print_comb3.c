8be6fb8a118c48afd3f5a12c36c77251d0a7b488
OttoNyongole <nyongole.otto@gmail.com>
2022-06-05 14:10:16 -0700

    possible combination of double digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,9 +15,9 @@
 int main(void)
 {
 	int i, j;
-for (i = 0; i <= 9; i++)
+for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 10; j++)
 		{
 		putchar((i % 10) + '0');
 		putchar((j % 10) + '0');
