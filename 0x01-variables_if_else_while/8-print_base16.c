0f8917a6b0cff2fc63c8512876ce05d03dfeb9b2
OttoNyongole <nyongole.otto@gmail.com>
2022-06-05 13:19:20 -0700

    printing hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+/**
+ * c file to print Hexadecimal
+ */
+#include <stdio.h>
+
+/**
+ * main a program that prints all the 
+ * numbers of base 16 in lowercase, followed by a new line.
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int i;
+	char letters;
+
+	for (i = 0; i < 10; i++)
+		putchar((i % 10) + '0');
+	for (letters = 'a' letters <= 'f'; letters++)
+		putchar(letters);
+	putchar('\n');
+	return (0);
+}
