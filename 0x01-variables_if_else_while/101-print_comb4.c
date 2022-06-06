5bd03bd2228876b54b32986d97a1e972c5839f56
Eng-Otto <nyongole.otto@gmail.com>
2022-06-06 15:08:09 +0300

    added line and space

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,6 +14,7 @@
 int main(void)
 {
 	int i, j, k;
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = i + 1; j < 9; j++)
@@ -33,6 +34,6 @@ int main(void)
 		}
 	}
 	
-	putchar('\n');
+	putchar ('\n');
 	return(0);
 }
