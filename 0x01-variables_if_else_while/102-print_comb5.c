84160d7781205b6fa76a18687b96f3bdcdefa1a4
Eng-Otto <nyongole.otto@gmail.com>
2022-06-06 15:37:52 +0300

    combination of two two-digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,13 +19,15 @@ int main(void)
 	{
 		for (j = i + 1; j <= 99; j++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % +10) + '0');
+			putchar((i/10) + '0');
+			putchar((i % + 10) + '0');
 			putchar(' ');
 			putchar((j / 10) + '0');
 			putchar((i % 10) + '0');
+			
 			if (i == 98 && j == 99)
 				continue;
+
 			putchar(',');
 			putchar(' ');
 		}
