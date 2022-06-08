d8a0fafdadb14553b060d31b01eff99b4ce34fb8
Eng-Otto <nyongole.otto@gmail.com>
2022-06-08 19:16:37 +0300

    function nested loops folder

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * main - print _putchar as the message.
+ *
+ * Return: 0 
+ */
+int main(void)
+{
+	int str[] = {10, 15, 20, 25, 22, 27, 30};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
+}
