4c0e09ca8166a5e44851a02216a15fe12ba07a4a
Eng-Otto <nyongole.otto@gmail.com>
2022-06-08 19:32:31 +0300

    printing alphabet using _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print alphabets in lowercase using _putchar.
+ *
+ * Return: Always 0.
+ *
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a' i<= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
