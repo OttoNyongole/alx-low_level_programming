b70a53f3f1726afb60ffd08c9b79fb0910be83f3
Eng-Otto <nyongole.otto@gmail.com>
2022-06-10 08:34:36 +0300

    added main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,22 +1,17 @@
-#ifndef _main_h_
-#define _main_h_
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-void pr_digit(int charcode, int fmt);
-int add(int a, int b);
-void pr_with_fmt(int charcode, int fmt);
-void print_to_98(int n);
-void pr_3_digit(int charcode, int fmt);
-void print_times_table(int n);
-void print_spaces(int n);
-void print_zero(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
