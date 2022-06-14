d657947dbb2c1f32e9cbdac8dc044288d179b210
Eng-Otto <nyongole.otto@gmail.com>
2022-06-14 08:42:25 +0300

    correction reset_to_98 file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  * reset_to_98 - takes a pointer and updates the value
- *   *@n:input
- *    * Return: Always 0.
+ * reset_to_98 - takes a pointer and updates the value
+ * @n:input
+ * Return: Always 0.
 */
 void reset_to_98(int *n)
 {
