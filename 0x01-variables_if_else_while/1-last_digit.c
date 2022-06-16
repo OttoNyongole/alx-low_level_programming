6467709e44f6ea5b438a9bbd263df4bd3293d081
Eng-Otto <nyongole.otto@gmail.com>
2022-06-16 11:51:49 +0300

    added 1-last_digit.c file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,32 @@
-#!/bin/bash
-#include <stdio.h>
-#include<conio.h>
 #include <stdlib.h>
-/**Declaration of function main
- *main function is the first function to be excuted by the program
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
  */
-int main ()
+int main(void)
 {
-	printf("The rondom number is: %d",rand());
-	printf("The rondom number is: %d",rand());
-	printf("The rondom number is: %d",rand());
-	printf("The rondom number is: %d",rand());
+	int n, lastd;
 
-return 0;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
+
+	if (lastd > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+	}
+	else if (lastd == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastd);
+	}
+	else if (lastd < 6 && lastd != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+	}
+
+	return (0);
 }
-
