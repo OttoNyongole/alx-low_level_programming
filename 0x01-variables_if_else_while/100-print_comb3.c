2294ad8bd7cd3264b968313424c1051955a3b64a
Eng-Otto <nyongole.otto@gmail.com>
2022-06-16 12:09:32 +0300

    added corrections

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,33 +1,37 @@
-/**
- * c file printing combination of dought digit
- *
- */
-
 #include <stdio.h>
 
 /**
- * main - a program that prints all possible 
- * different combinations of two digits
+ * main - Prints numbers between 00 to 89.
  *
- * Return:Always zero
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int i, j;
-for (i = 0; i < 10; i++)
+	int i, e;
+
+	i = 48;
+	e = 48;
+
+	while (e < 58)
 	{
-		for (j = 0; j < 10; j++)
+		i = 48;
+		while (i < 58)
 		{
-		putchar((i % 10) + '0');
-		putchar((j % 10) + '0');
-		if (i == 9 && j == 9)
-			continue;
-		putchar(',');
-		putchar(' ');
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
 		}
+		e++;
 	}
-	
 	putchar('\n');
-return (0);
+	return (0);
 }
