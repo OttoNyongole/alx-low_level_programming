8445ff076764a5227c0e395102428c7d1869b13a
Eng-Otto <nyongole.otto@gmail.com>
2022-06-16 11:45:32 +0300

    added correction of 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- *  * main - Prints out the last part of a quote in the standard error.
- *   * Return: 1 if success.
- *    */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-			return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
