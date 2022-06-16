b64c3439b7645ac393b5e323c737adc675fd9347
Eng-Otto <nyongole.otto@gmail.com>
2022-06-16 11:39:38 +0300

    correction of 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc -S -m32 $CFILE
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
