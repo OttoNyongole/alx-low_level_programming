7d547e5545d8d20a818af99d6ce6c12be7eaad68
Eng-Otto <nyongole.otto@gmail.com>
2022-06-22 17:33:44 +0300

    correction factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,12 +7,13 @@
  */
 int factorial(int n)
 {
-	if (n <= 0)
+	if (n < 0)
 	{
 		return (-1);
 	}
-	else
+	if (n == 1)
 	{
-		return (n * factorial(n - 1));
+		return (1);
 	}
+	return (n * factorial(n - 1));
 }
