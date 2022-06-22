88a0b8415adb8f61ba9ee888381720877d792fcc
Eng-Otto <nyongole.otto@gmail.com>
2022-06-22 21:45:42 +0300

    printing prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+int tmp_prime(int n, int i);
+
+/**
+ * divisors - check the number weather its prime.
+ * @n: integer parameter
+ * @m: integer parameter
+ * Return: boolean
+ */
+
+int divisors(int n, int m)
+{
+	if (m % n == 0)
+	{
+		return (0);
+	}
+	else if (m / 2 > n)
+	{
+		return (divisors(n + 2, m));
+	}
+	else
+	{
+		return (1);
+	}
+}
+
+/**
+ * is_prime_number - prints the prime number
+ * @n: integer parameter
+ * Return: recursion
+ */
+
+int is_prime_number(int n)
+{
+	if ((!(n % 2) && n != 2) || n < 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (divisors(3, n));
+	}
+}
