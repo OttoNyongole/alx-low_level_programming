3e2445bb470e24d28745a33e5bfbd08d7b80238c
Eng-Otto <nyongole.otto@gmail.com>
2022-06-28 18:28:42 +0300

    corrected array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 	return (0);
 	p = malloc((size) * sizeof(char));
-	if (p == '\0')
+	if (p = '\0')
 	return (0);
 	for (i = 0; i < size; i++)
 	{
