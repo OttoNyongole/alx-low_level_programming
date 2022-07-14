258c93cf24aaa409fef790480d0d7e14a71fe734
Eng-Otto <nyongole.otto@gmail.com>
2022-07-14 20:14:12 +0300

    modifications lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _lists_h
-#define _lists_h
+#ifndef LISTS_H
+#define LISTS_H
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
