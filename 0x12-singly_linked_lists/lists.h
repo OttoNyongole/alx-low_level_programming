9eb37f6ec0e9242e5bf5443e8a195fded6717909
Eng-Otto <nyongole.otto@gmail.com>
2022-07-14 19:53:51 +0300

    added lists.h fie

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,10 @@
+#ifndef _lists_h
+#define _lists_h
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+int _putchar(char c)
+
+#endif
