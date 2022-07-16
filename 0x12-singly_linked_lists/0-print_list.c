985cf963d19d3312164446a5f5d0dde3e06ea850
OttoNyongole <nyongole.otto@gmail.com>
2022-07-16 11:07:56 +0300

    removed trailling white space

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,11 +9,10 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
-	
+	size_t nodes = 0;	
 	while (h)
 	{
-		if (h->str==NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else 
 			printf("[%d] %s\n", h->len, h->str);
