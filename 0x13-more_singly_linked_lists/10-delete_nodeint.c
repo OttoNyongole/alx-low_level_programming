e6becce2db6dd0c596ddb7f1b2e1e6ae873ec844
Eng-Otto <nyongole.otto@gmail.com>
2022-07-19 22:37:27 +0300

    corrections header

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,48 +1,50 @@
-#include <stdio.h>
 #include "lists.h"
-
 /**
- * delete_nodeint_at_index - a function that deletes the node at
- * index of a linked list
- * @head: a pointer to the list
- * @index: the index of the node that should be deleted, starts at 0
+ * listint_len - returns the number of elements in a linkedlisti\nt_t list
+* @h: a variable that points to a listint_t struct
+* Return: number of elements
+*/
+size_t listint_len(const listint_t *h)
+{
+	int counter = 0;
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
+/**
+ * delete_nodeint_at_index - deletes the node at index of a listint_t list
+ * @head: pointer to pointer to listint_t struct
+ * @index: index of node that should be deleted
  *
  * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i = 0;
-	listint_t *temp;
-	listint_t *delete = NULL;
+	size_t length;
+	unsigned int i;
+	listint_t *prev_node = *head;
+	listint_t *tmp;
 
-	if (head == NULL || (*head) == NULL)
+	length = listint_len(*head);
+	if (index >= length)
 		return (-1);
-
-	temp = *head;
-
 	if (index == 0)
 	{
-		*head = temp->next;
-		free(temp);
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
 		return (1);
 	}
 
-	while (index != i && temp != NULL)
-	{
-		delete = temp;
-		temp = temp->next;
-		i += 1;
-	}
-
-	if (index != i)
-	{
-		return (-1);
-	}
-
-	if (delete == NULL)
-		return (-1);
-
-		delete->next = temp->next;
-		free(temp);
-		return (1);
+	for (i = 0; i < index - 1; i++)
+		prev_node = prev_node->next;
+	tmp = prev_node;
+	prev_node = prev_node->next;
+	tmp->next = prev_node->next;
+	free(prev_node);
+	return (1);
 }
