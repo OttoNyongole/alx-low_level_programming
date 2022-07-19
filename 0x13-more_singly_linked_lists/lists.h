b7370b78e1a86e5bf46e1a64edba72751812e9c1
Eng-Otto <nyongole.otto@gmail.com>
2022-07-19 23:03:06 +0300

    modified header file lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -28,5 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+size_t free_listint_safe(listint_t **h);
+size_t print_listint_safe(const listint_t *head);
 
 #endif
