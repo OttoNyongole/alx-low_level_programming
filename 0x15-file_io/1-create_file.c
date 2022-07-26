1f416319f540ce41d7010e1aef7c012ec559bb70
Eng-Otto <nyongole.otto@gmail.com>
2022-07-26 15:57:18 +0300

    white space error correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,35 @@
 #include "main.h"
+
 /**
- * create_file - creates a file and fills it with text
- * @filename: name of the file to create
- * @text_content: text to write in the file
+ * create_file - creates a file
+ * @filename: the name of the file to create
+ * @text_content: null-terminated string to write to file
  *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, t, s = 0;
+	size_t i = 0;
+	ssize_t writecount;
+	int fd;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd < 0)
-		return (-1);
-
-	if (text_content)
+	if (text_content == NULL)
 	{
-		while (text_content[s])
-			s++;
-		t = write(fd, text_content, s);
-		if (t != s)
+		fd = open(filename, O_CREAT, 0600);
+		if (fd == -1)
 			return (-1);
+		return (1);
 	}
-
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	while (text_content[i] != 0)
+		i++;
+	writecount = write(fd, text_content, i);
+	if (writecount == -1)
+		return (-1);
 	close(fd);
-
-	return (-1);
+	return (1);
 }
